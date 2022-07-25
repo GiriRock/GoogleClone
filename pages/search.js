3071bb23354759370fb97b443283088b517aab9c
Giridharan <giridharanrock@gmail.com>
2022-07-25 22:16:36 +0530

    search page

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,38 @@
 import Head from 'next/head'
-function Search() {
+import Header from '../components/Header'
+import response from '../response'
+import {useRouter} from 'next/router'
+import SearchResults from '../components/SearchResults'
+function Search({results}) {
+  const router = useRouter()
+  console.log(results)
   return (
     <div>
         <Head>
-            <title>Search Results</title>
+            <title>{router.query.q} - Google Search</title>
             <link rel="icon" href="/logo.ico" />
-        </Head>        
+        </Head>
+        <Header />
+        <SearchResults results={results}/>
     </div>
   )
 }
 
 export default Search
+
+export async function getServerSideProps(context){
+  const useDummyData  = false
+  const startIndex = context.query.start || '0'
+  const key = process.env.API_KEY
+  
+  const data = useDummyData ? response : await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${process.env.CONTEXT_KEY}
+    &q=${context.query.q}&start=${startIndex}`
+  ).then(response=>response.json())
+
+  return{
+    props:{
+      results : data
+    }
+  }
+}
