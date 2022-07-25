9e6dcbef764f1930a787213e06630ce44ee1a1fa
Giridharan <giridharanrock@gmail.com>
2022-07-25 19:09:53 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 
         sm:space-y-0 sm:flex-row sm:space-x-4">
           <button onClick={search} className="button">Google search</button>
-          <button onClick={search} className="button">I'm feeling lucky</button>
+          <button onClick={search} className="button">Im feeling lucky</button>
         </div>
       </form>
       <Footer/>
